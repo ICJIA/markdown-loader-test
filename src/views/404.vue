b1a3d2897e4fc7cc242fda7e76de493a3cbb84ab
cschweda <cschweda@gmail.com>
2020-01-22 14:50:45 -0600

    feat: style 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>
-    <h1>404 -- Page Not Found</h1>
-  </div>
+  <v-container
+    class="text-center"
+    fill-height
+    style="height: calc(100vh - 148px);"
+  >
+    <v-row align="center">
+      <v-col>
+        <h1 class="display-2 primary--text">404</h1>
+
+        <p>The page you were looking for does not exist</p>
+
+        <v-btn :to="`/`" color="primary" outlined>
+          Go Home
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
+  name: "FourOhFour",
+
+  props: {
+    lang: {
+      type: String,
+      default: "en"
+    }
+  },
+
   created() {}
 };
 </script>
-
-<style lang="scss" scoped></style>
